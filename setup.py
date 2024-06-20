de65a352d2e6bef0419abf281bc7f9d65c3b7343
Muhammad Abrar <legendabrar44@gmail.com>
2024-06-20 17:05:14 +0500

    Structured as package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='colrfx',
+    version='0.1.0',
+    description='Python utilities for color handling (RGB, HEX) and visualization.',
+    author='Muhammad Abrar',
+    author_email='legendabrar44@gmail.com',
+    url='https://github.com/abrarishere/colrfx',
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)
